0b6421213249df1aa8577d533ad3496447a7abe4
GabeOP <gabrieloliveirap1@hotmail.com>
2023-07-18 14:15:07 -0300

    feat: GenreNotFoundException

--- src/main/java/com/gabriel/demo/model/exception/GenreNotFoundException.java
+++ src/main/java/com/gabriel/demo/model/exception/GenreNotFoundException.java
@@ -1,5 +1,10 @@
 package com.gabriel.demo.model.exception;
 
-public class GenreNotFoundException {
+public class GenreNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public GenreNotFoundException(String msg) {
+		super(msg);
+	}
 
 }
